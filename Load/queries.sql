6b331c89cb3d9d480060e5453898b4a190649f03
kfmatovic716 <jabuka611@gmail.com>
2021-03-26 21:53:13 -0700

    Finish running SQL queries

--- Load/queries.sql
+++ Load/queries.sql
@@ -2,111 +2,156 @@
 SELECT
 	*
 FROM
-	Vaccine;
+	"Vaccine";
 
 SELECT
 	*
 FROM
-	Deaths;
+	"Deaths";
 
 SELECT
 	*
 FROM
-	Total_tests;
+	"Total_tests";
 
 SELECT
 	*
 FROM
-	Hospitalized_COVID_patients;
+	"Hospitalized_COVID_patients";
 
 SELECT
 	*
 FROM
-	Cases;
+	"Cases";
 
+SELECT
+	*
+FROM
+	"ICU_COVID_patients";
+
+SELECT
+	*
+FROM
+	"Hospital_beds";
 
 -- Count number of records in employees and salaries table before joining
 SELECT
 	COUNT(*)
 FROM
-	Vaccine;
+	"Vaccine";
 	
 SELECT
 	COUNT(*)
 FROM
-	Deaths;
+	"Deaths";
 
 SELECT
 	COUNT(*)
 FROM
-	Total_tests;
+	"Total_tests";
 
 SELECT
 	COUNT(*)
 FROM
-	Hospitalized_COVID_patients;
+	"Hospitalized_COVID_patients";
 
 SELECT
 	COUNT(*)
 FROM
-	Cases;
+	"Cases";
+
+SELECT
+	COUNT(*)
+FROM
+	"ICU_COVID_patients";
+
+SELECT
+	COUNT(*)
+FROM
+	"Hospital_beds";
 
 
-
--- Total number of vaccine doses administered per day
+-- Total number of vaccine doses administered per county in descending order
+-- to see which county has the highest number of administered vaccine
 SELECT 
-	SUM(Vaccine.total_doses)
+	"Vaccine".county,
+	SUM ("Vaccine".total_doses) AS total_vaccine_doses
 FROM 
-	Vaccine
-ORDER BY
-	Vaccine.date ASC;
+	"Vaccine"
+GROUP BY
+	"Vaccine".county
+ORDER BY 
+	total_vaccine_doses DESC;
 
 
--- Average Reported Deaths in each county
+-- Total number of deaths in Los Angeles county. This also shows which dates did the 
+-- Los Angeles incur the most deaths. 
 SELECT 
-	AVG(Deaths.reported_deaths)
+	"Deaths".county,
+	"Deaths".date,
+	SUM ("Deaths".deaths) AS total_deaths
 FROM 
-	Deaths
-ORDER BY
-    Deaths.county ASC;
-
-
--- Total number of Hospitalized COVID patients in Los Angeles & Ventura counties per day
-SELECT 
-	Hospitalized_COVID_patients.county,
-	Hospitalized_COVID_patients.date,
-	SUM(Hospitalized_COVID_patients.total_covid_patients) 
-FROM 
-	Hospitalized_COVID_patients
+	"Deaths"
 WHERE
-	Hospitalized_COVID_patients.county = 'Los Angeles'
-    AND Hospitalized_COVID_patients.county = 'Ventura'
+	"Deaths".county = 'Los Angeles'
+GROUP BY
+	"Deaths".county,
+	"Deaths".date
+ORDER BY 
+	total_deaths DESC;
+
+
+-- Average number of Hospitalized COVID patients in Los Angeles when COVID just started (March 1, 2020- March 31,2020)
+SELECT
+	"Hospitalized_COVID_patients".county,
+	"Hospitalized_COVID_patients".date,
+	ROUND(AVG("Hospitalized_COVID_patients".total_covid_patients), 2) AS avg_hospitalized_patients
+FROM
+	"Hospitalized_COVID_patients"
+WHERE
+	"Hospitalized_COVID_patients".county = 'Los Angeles'
+	AND "Hospitalized_COVID_patients".date between '2020-03-01' and '2020-03-31'
+GROUP BY
+	"Hospitalized_COVID_patients".county,
+	"Hospitalized_COVID_patients".date
 ORDER BY
-    Hospitalized_COVID_patients.date;
+	avg_hospitalized_patients DESC;
 
 
 -- Which county  has the most positive tests?
 SELECT
-    Total_tests.county,
-    Total_tests.date,
-    MAX(Total_tests.positive_tests) AS max_positive_tests
+	"Total_tests".county,
+    "Total_tests".date,
+    MAX("Total_tests".positive_tests) AS max_positive_tests
 FROM
-    Total_tests
+	"Total_tests"
 GROUP BY
-    Total_tests.county;
+	"Total_tests".county,
+	"Total_tests".date
+ORDER BY
+	max_positive_tests DESC;
 
+-- Create sample view of some joined tables: ICU_COVID_patients and Hospital_beds 
+CREATE VIEW icu_patients_and_beds AS (
+	SELECT	
+		"ICU_COVID_patients".county,
+		"ICU_COVID_patients".date,
+		"ICU_COVID_patients".icu_covid_confirmed,
+		"ICU_COVID_patients".icu_covid_suspected,
+		"Hospital_beds".all_hospital_beds,
+		"Hospital_beds".icu_available_beds
+	FROM
+		"ICU_COVID_patients"
+	INNER JOIN "Hospital_beds" ON
+		("ICU_COVID_patients".county, "ICU_COVID_patients".date) = ("Hospital_beds".county, "Hospital_beds".date)
+);
 
--- Join tables: Vaccine and Cases 
+-- Query the table view created
 SELECT 
-	Vaccine.county,
-	Vaccine.date,
-	Vaccine.total_doses,
-	Cases.county,
-    Cases.date,
-	Cases.reported_cases
+  *
 FROM 
-	Vaccine
-INNER JOIN Cases ON
-	(Vaccine.county, Vaccine.date) = (Cases.county, Cases.date)
-    
+  icu_patients_and_beds;
+
+-- Drop view
+-- DROP VIEW icu_patients_and_beds;
 
